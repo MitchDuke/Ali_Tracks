e94fa5ab840ad5a3cd978598d53b48f63e2d2216
MitchDuke <mdmsduke@googlemail.com>
2024-04-27 10:21:53 +0000

    added app routes for retrieving current user goals

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/budget_tracker"
 mongo = PyMongo(app)
 
+
+@app.route('/')
+def index():
+    return 'Welcome to Budget Tracker'
+
+
+# Route for creating a new goal
 @app.route('/goal', methods=['POST'])
 def create_goal():
     data = request.json
@@ -13,7 +20,6 @@ def create_goal():
     current_amount = data.get('current_amount', 0)
     user_id = data.get('user_id')
 
-
     goal = {
         'title': title,
         'target_amount': target_amount,
@@ -21,14 +27,20 @@ def create_goal():
         'user_id': user_id
     }
 
-
     mongo.db.goals.insert_one(goal)
     return jsonify({'message': 'Goal created successfully'}), 201
 
 
-@app.route('/')
-def index():
-    return 'Welcome to Budget Tracker'
+# Route for getting all goals for a user
+@app.route('/goals/<user_id>', methods=['GET'])
+def get_goals(user_id):
+    goals = mongo.db.goals.find({'user_id: user_id'})
+    result = []
+    for goal in goals:
+        goal['_id'] = str(goal['_id'])
+        result.append(goal)
+    return jsonify(result), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
