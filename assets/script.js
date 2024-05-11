0fcaa5f6de583d8666e87185e65ae0f511235875
MitchDuke <mdmsduke@googlemail.com>
2024-05-11 11:51:55 +0000

    added function for button clicks fetch the total and update the total money saved

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,23 @@
 
 async function addAmount(month) {
-    const button = document.getElementById(month):
+    const button = document.getElementById(month);
+    if (button.disabled) return; // If button is disabled, do nothing
+
+    const response = await fetch('/update_total', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ month: month})
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        document.getElementById('total').textContent = `Total: £${data.total}`;
+        button.disabled = true; // Disable the button after it's clicked
+        button.classList.add('btn-secondary'); // Change button color or style to indicate it has been clicked
+
+    }  else {
+        console.error('Failed to update total');
+    }
 }
