54c1856376cab8739e46e38f4c241a9f1dd30a48
MitchDuke <mdmsduke@googlemail.com>
2024-05-26 10:33:22 +0000

    corrected errors to onClick within html, added window onload async function to script.js to check for response

--- assets/script.js
+++ assets/script.js
@@ -15,9 +15,20 @@ async function addAmount(month) {
         const data = await response.json();
         document.getElementById('total').textContent = `Total: £${data.total}`;
         button.disabled = true; // Disable the button after it's clicked
+        button.classList.remove('btn-primary');
         button.classList.add('btn-secondary'); // Change button color or style to indicate it has been clicked
 
     }  else {
         console.error('Failed to update total');
     }
 }
+
+window.onload = async function() {
+    const response = await fetch('/get_total');
+    if (response.ok) {
+        const data = await response.json();
+        document.getElementById('total').textContent = data.total;
+    } else {
+        console.error('Failed to fetch total');
+    }
+};
